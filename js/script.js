400001e809f9b69abe88c902eceb4b85f4e3e886
lakshmi1970 <50042155+lakshmi1970@users.noreply.github.com>
2024-10-28 16:57:26 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(window).on('load', function() {
+    var $container = $('.portfolioContainer');
+    var $filter = $('#filter');
+    $container.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+    $filter.find('a').click(function() {
+        var selector = $(this).attr('data-filter');
+        $filter.find('a').removeClass('active');
+        $(this).addClass('active');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                animationDuration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+        return false;
+    });
+});
+ 
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: $('.slick-prev'),
+    nextArrow: $('.slick-next'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+              
+$('.destination .slick-prev, .destination .slick-next').addClass('d-lg-block d-none');
+
+  AOS.init();
